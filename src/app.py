11fe17a50a2381bc2b065a7b4b7748c03826faaa
bjasinsk <01169292@pw.edu.pl>
2024-02-29 19:40:10 +0100

    add POST /rate, GET /cars, GET /popular endpoints

--- src/app.py
+++ src/app.py
@@ -16,6 +16,10 @@ def add_car():
     make = data.get('make')
     model = data.get('model')
 
+    existing_car = Car.query.filter_by(make=make, model=model).first()
+    if existing_car:
+        return jsonify({'ERROR': 'Car already exists'}), 400
+
     response = requests.get(f"https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/{make}?format=json")
     cars = response.json().get('Results')
     if not any(car['Model_Name'].lower() == model.lower() for car in cars):
@@ -24,8 +28,70 @@ def add_car():
     new_car = Car(make=make, model=model)
     db.session.add(new_car)
     db.session.commit()
-
     return jsonify({'SUCCESS': 'Car was added to the database'}), 201
+
+@app.route('/rate', methods=['POST'])
+def rate_car():
+    data = request.json
+    make = data.get('make')
+    model = data.get('model')
+    rating = data.get('rating')
+
+    if make is None:
+        return jsonify({'ERROR': 'Missing make'}), 400
+
+    if model is None:
+        return jsonify({'ERROR': 'Missing model'}), 400
+
+    if rating is None:
+        return jsonify({'ERROR': 'Missing rating'}), 400
+
+    try:
+        rating = int(rating)
+    except ValueError:
+        return jsonify({'ERROR': 'Rating must be an integer'}), 400
+
+    if not (1 <= rating <= 5):
+        return jsonify({'ERROR': 'Rating must be between 1 and 5'}), 400
+
+    car = Car.query.filter_by(make=make, model=model).first()
+    if car:
+        total_rating = car.average_rate * car.vote_count + rating
+        car.vote_count += 1
+        car.average_rate = total_rating / car.vote_count
+        db.session.commit()
+        return jsonify({'SUCCESS': f'{make} {model} rated with {rating}'}), 200
+    else:
+        return jsonify({'ERROR': 'Car does not exist'}), 404
+
+@app.route('/cars', methods=['GET'])
+def get_cars():
+    cars = Car.query.all()
+    cars_list = []
+    for car in cars:
+        cars_list.append({
+            'id': car.id,
+            'make': car.make,
+            'model': car.model,
+            'average_rate': car.average_rate,
+            'votes': car.vote_count
+        })
+    return jsonify(cars_list), 200
+
+@app.route('/popular', methods=['GET'])
+def get_popular_cars():
+    cars = Car.query.order_by(Car.vote_count.desc()).all()
+    popular_cars_list = []
+    for car in cars:
+        popular_cars_list.append({
+            'id': car.id,
+            'make': car.make,
+            'model': car.model,
+            'average_rate': car.average_rate,
+            'votes': car.vote_count
+        })
+    return jsonify(popular_cars_list), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/car.py
+++ src/car.py
@@ -8,3 +8,7 @@ class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     make = db.Column(db.String(100), nullable=False)
     model = db.Column(db.String(100), nullable=False)
+    average_rate = db.Column(db.Float, default=0)
+    vote_count = db.Column(db.Integer, default=0)
+
+    __table_args__ = (db.UniqueConstraint('make', 'model', name='unique_make_model'),)
